a3fbe56b932c2607cbe2f222234c097765d7641b
Satrya Rangga Buana <satrya.rangga@olx.co.id>
2017-07-22 21:04:23 +0700

    add routing checkout

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::get('promo', 'AjaxController@getHotelPromo');
 Route::get('detailhotel', 'AjaxController@getHotelDetail');
 Route::get('order', 'AjaxController@getHotelOrder');
 Route::get('addOrder', 'AjaxController@addHotelOrder');
+Route::get('checkout', 'AjaxController@checkoutCustomer');
