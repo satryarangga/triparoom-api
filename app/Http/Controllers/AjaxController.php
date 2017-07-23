a9114c51cdfbffd6857f86477a1aaf983658f2b4
Satrya Rangga Buana <satrya.rangga@olx.co.id>
2017-07-23 15:56:07 +0700

    bank transfer

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -99,12 +99,15 @@ class AjaxController extends Controller {
     $phone = $request->input('phone');
     $order_id = $request->input('order_id');
     $detail_id = $request->input('order_detail_id');
-    
-    $res = $client->request('GET', $this->url.'/order/checkout/'.$order_id.'/IDR?token='.$token.'&output='.$this->output);
-    $res = $client->request('GET', $this->url.'/checkout/checkout_customer?token='.$token.'&output='.$this->output.'&salutation='.$salute.'&firstName='.$firstName.'&lastName='.$lastName.'&phone='.$phone.'&emailAddress='.$email.'&conSalutation='.$salute.'&conFirstName='.$firstName.'&conLastName='.$lastName.'&conEmailAddress='.$email.'&conPhone='.$phone.'&detailId='.$detail_id.'&country=id&saveContinue=2');
-    if($res->getStatusCode() == 200){
-      $res = $client->request('GET', $this->url.'/checkout/checkout_customer?token='.$token.'&output='.$this->output.'&salutation='.$salute.'&firstName='.$firstName.'&lastName='.$lastName.'&phone='.$phone.'&emailAddress='.$email.'&conSalutation='.$salute.'&conFirstName='.$firstName.'&conLastName='.$lastName.'&conEmailAddress='.$email.'&conPhone='.$phone.'&detailId='.$detail_id.'&country=id');
+    $uri = $request->input('uri');
+    $uri = urldecode($uri);
+
+    $res = $client->request('GET', $this->url.'/checkout/checkout_customer?token='.$token.'&output='.$this->output.'&salutation='.$salute.'&firstName='.$firstName.'&lastName='.$lastName.'&phone='.$phone.'&emailAddress='.$email.'&conSalutation='.$salute.'&conFirstName='.$firstName.'&conLastName='.$lastName.'&conEmailAddress='.$email.'&conPhone='.$phone.'&detailId='.$detail_id.'&country=id');
+    // $res = $client->request('GET', $this->url.'/checkout/checkout_customer?token='.$token.'&output='.$this->output.'&salutation='.$salute.'&firstName='.$firstName.'&lastName='.$lastName.'&phone='.$phone.'&emailAddress='.$email.'&conSalutation='.$salute.'&conFirstName='.$firstName.'&conLastName='.$lastName.'&conEmailAddress='.$email.'&conPhone='.$phone.'&detailId='.$detail_id.'&country=id&saveContinue=2');
+    if(stristr($string, '?')) { // processed by own website ex: bank transfer
+      $client->request('GET', $uri.'?token='.$token.'&output='.$this->output);
     }
+
     return $res->getBody();
   }
 
