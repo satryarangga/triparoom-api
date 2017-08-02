2ba49122033d745df274f6868df8d13b1a1ed794
Satrya Rangga Buana <satrya.rangga@olx.co.id>
2017-08-02 23:28:36 +0700

    param missing

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -164,8 +164,13 @@ class AjaxController extends Controller {
     $lastName = $request->input('last_name');
     $email = $request->input('email');
     $phone = $request->input('phone');
+    $flightId = $request->input('flight_id');
+    $retFlightId = $request->input('ret_flight_id');
+    $adult = $request->input('adult');
+    $child = $request->input('child');
+    $infant = $request->input('infant');
 
-    $res = $client->request('GET', $this->url."/order/add/flight?token=$token&conSalutation=$salute&conFirstName=$firstName&conLastName=$lastName&conPhone=$phone&conEmailAddress=$email&titlea1=$salute&firstnamea1=$firstName&lastnamea1=$lastName&birthdatea1=1990-01-01&passportnationalitya1=ID&dcheckinbaggagea11=15&output=".this->output);
+    $res = $client->request('GET', $this->url."/order/add/flight?token=$token&conSalutation=$salute&conFirstName=$firstName&conLastName=$lastName&conPhone=$phone&conEmailAddress=$email&titlea1=$salute&firstnamea1=$firstName&lastnamea1=$lastName&birthdatea1=1990-01-01&passportnationalitya1=ID&dcheckinbaggagea11=15&output=".$this->output."&flight_id=$flightId&ret_flight_id=$retFlightId&adult=$adult&child=$child&infant=$infant");
 
     return $res->getBody();
   }
